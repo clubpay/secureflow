369587e34896d8c210b2a52ba014f80dfdefcdd2
Farshid Sanei <83496727+farsheedify@users.noreply.github.com>
2025-09-29 12:11:46 +0330

    Update test.go

--- test.go
+++ test.go
@@ -177,7 +177,7 @@ func (m *acl) ApplyPayment(ctx context.Context, orderID, txnID, total, bill, tip
 		return fmt.Errorf("orderId %d out of int32 range", checkSeq)
 	}
 
-	checkSeqCasted := int32(checkSeq)
+	checkSeqCasted := int32(checkSeq) // nosemgrep
 	err := m.addPayment(ctx, checkSeqCasted,txnID, bill, tip)
 	if err != nil {
 		m.lg.ErrorCtx(ctx, "error on addPayment", log.Error(err))
